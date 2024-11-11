30e0336ee582228b4696429b72e56c64d41f911f
Alex Rivard <alex.rivard4406@gmail.com>
2024-11-11 10:23:22 -0600

    accounts table replaced with user table

--- database.sql
+++ database.sql
@@ -2,11 +2,6 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
 
 CREATE TABLE "Locations" (
 	"id" SERIAL PRIMARY KEY,
@@ -20,7 +15,8 @@ CREATE TABLE "Spaces" (
 	"location_id" INTEGER REFERENCES "Locations"
 );
 
-CREATE TABLE "Account" (
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
 	"username" VARCHAR(40) UNIQUE,
 	"password" VARCHAR(300) NOT NULL,
 	"isAdmin" BOOLEAN DEFAULT FALSE 
