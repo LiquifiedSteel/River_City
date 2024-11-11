8f9ef111e2536f09d472bfa57eb35b0a4a4ccdd9
Alex Rivard <alex.rivard4406@gmail.com>
2024-11-11 14:01:25 -0600

    admin router setup alonf with some authentication stuff for use in the future if we ever need it

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -1,9 +1,12 @@
 const rejectUnauthenticated = (req, res, next) => {
   // check if logged in
   if (req.isAuthenticated()) {
-    // They were authenticated! User may do the next thing
-    // Note! They may not be Authorized to do all things
-    next();
+    // They are logged in! Now check if they are admin.
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.sendStatus(403);
+    }
   } else {
     // failure best handled on the server. do redirect here.
     res.sendStatus(403);
