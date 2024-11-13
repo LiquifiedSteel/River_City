5d221573857ba901ddb1ebed742e6344407552ec
Alex Rivard <alex.rivard4406@gmail.com>
2024-11-13 13:29:34 -0600

    now registration doesn't need email, just remember to remove NOT NULL from the email comlumn of user

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -23,7 +23,7 @@ router.post('/register', (req, res, next) => {
   const password = encryptLib.encryptPassword(req.body.password);
   // const email = req.body.email;
 
-  const queryText = `INSERT INTO "user" (username, password, email)
+  const queryText = `INSERT INTO "user" (username, password)
     VALUES ($1, $2) RETURNING id`;
   pool
     .query(queryText, [username, password])
