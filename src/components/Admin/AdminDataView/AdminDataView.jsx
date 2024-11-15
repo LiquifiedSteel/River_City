0ea8c98ef76e3886939ed74696aaa828419bf800
Alex Rivard <alex.rivard4406@gmail.com>
2024-11-14 23:47:12 -0600

    Admin Data View component has been built, data is inserted correctly.

--- src/components/Admin/AdminDataView/AdminDataView.jsx
+++ src/components/Admin/AdminDataView/AdminDataView.jsx
@@ -36,9 +36,48 @@ function AdminDataView() {
         <Col xs={6} md={4}>Title with Team/ Organization/ Event: {request.title_w_team_org_event}</Col>
         <Col xs={6} md={4}>Coach's Name: {request.coach_contact_first_name} {request.coach_contact_last_name}</Col>
         <Col xs={6} md={4}>Coach's Email: {request.coach_contact_email}</Col>
-        <Col xs={6} md={4}>{request.coach_contact_phone}</Col>
-        <Col xs={6} md={4}>{request.website}</Col>
+        <Col xs={6} md={4}>Coach's Phone #: {request.coach_contact_phone}</Col>
+        <Col xs={6} md={4}>Related Website: {request.website}</Col>
 
+        
+        <Col xs={12}>Event Description: {request.eventDescription}</Col>
+        <Col xs={6} md={4}>Type of Event: {request.event_type}</Col>
+        <Col xs={6} md={4}>Preferred Timeframe: {request.preferredTime_start}</Col>
+        <Col xs={6} md={4}>Preferred Location (Primary Option): {request.preferred_location_primary}</Col>
+        <Col xs={6} md={4}>Preferred Location (Secondary Option): {request.preferred_location_secondary}</Col>
+        <Col xs={6} md={4}>Preferred Space: {request.preferred_space}</Col>
+        <Col xs={6} md={4}>Expected Attendance: {request.expected_attendance}</Col>
+        {/* <Col xs={6} md={4}>Age Group: {request.ageGroup}</Col> */}
+        <Col xs={6} md={4}>Preferred Days: {request.preferred_days}</Col>
+        <Col xs={6} md={4}>Priority: {request.priority}</Col>
+        <Col xs={6} md={4}>Specific Area: {request.specificAreas}</Col>
+        {/* <Col xs={6} md={4}>Alternative Choices: {request.alternativeChoices}</Col> */}
+        <Col xs={6} md={4}>Start Date: {request.start_date}</Col>
+        <Col xs={6} md={4}>End Date: {request.end_date}</Col>
+        <Col xs={6} md={4}>Additional Dates: {request.additional_dates}</Col>
+        <Col xs={6} md={4}>Frequency: {request.frequency}</Col>
+
+
+        <Col xs={6} md={4}>85% West Fargo Students?: {request.wf_students}</Col>
+        <Col xs={6} md={4}>Grade Level: {request.grade_level}</Col>
+        <Col xs={6} md={4}>Cloudinary Link to Roster PDF: {request.team_pdf}</Col>
+        {/* <Col xs={6} md={4}>Liability  Proof: {request.liabilityProof}</Col> */}
+        {/* <Col xs={6} md={4}>District Acknowledgment: {request.districtAcknowledgment}</Col>
+        <Col xs={6} md={4}>Special Requests: {request.specialRequests}</Col> */}
+        <Col xs={6} md={4}>Rented Previously: {request.rented_previously}</Col>
+        {/* <Col xs={6} md={4}>Frequency: {request.read_Rental_Review}</Col> */}
+        <Col xs={12}></Col>
+        <br />
+        <Col xs={6} md={4}>Renter's Name: {request.renter_first_name} {request.renter_last_name}</Col>
+        <Col xs={6} md={4}>Renter's Street Address: {request.renter_street_address}</Col>
+        <Col xs={6} md={4}>Renter's City: {request.renter_city}</Col>
+        <Col xs={6} md={4}>Renter's State: {request.renter_state}</Col>
+        <Col xs={6} md={4}>Renter's ZIP code: {request.renter_zip}</Col>
+        <Col xs={6} md={4}>Renter's Phone #: {request.renter_phone}</Col>
+        <Col xs={6} md={4}>Renter's Email: {request.renter_email}</Col>
+        {/* <Col xs={6} md={4}>Frequency: {request.agreeToRespectfulUseOfSpace}</Col>
+        <Col xs={6} md={4}>Frequency: {request.agreeToInvoicePaymentProcess}</Col> */}
+    
       </Row>
     </Container>
   );
