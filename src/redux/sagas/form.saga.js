723e18c0cd4932d8ea15e6a6852d491b1ffdddd9
padifa <padifa7@live.fr>
2024-11-19 15:30:34 -0600

    cloudinary fully implemented and linked to the database

--- src/redux/sagas/form.saga.js
+++ src/redux/sagas/form.saga.js
@@ -8,34 +8,11 @@ function* submitFormSaga(action) {
     console.log(formData.formPartTwo);
     console.log(formData.formPartTwo.team_pdf);
 
-    // ====== Handle file uploads (e.g., roster and liabilityProof) ======
-    const uploadFile = async (file) => {
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", "your_upload_preset");
-      const response = await axios.post(
-        "https://api.cloudinary.com/v1_1/your_cloud_name/upload",
-        formData
-      );
-      return response.data.url;
-    };
-
-    // ====== Upload files if they exist ======
-    const rosterUrl = formData.formPartTwo.team_pdf
-      ? yield call(uploadFile, formData.formPartTwo.team_pdf)
-      : null;
-
-    const liabilityProofUrl = formData.formPartThree.liabilityProof
-      ? yield call(uploadFile, formData.formPartThree.liabilityProof)
-      : null;
-
     // ====== Prepare the final payload ======
     const finalPayload = {
       ...formData.formPartOne,
       ...formData.formPartTwo,
       ...formData.formPartThree,
-      rosterUrl,
-      liabilityProofUrl,
     };
     console.log(finalPayload);
     const response = yield call(axios.post, "/api/application", finalPayload);
