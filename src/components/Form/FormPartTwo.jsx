46386246c55a5317eb3c7f1348ff65bdc1004bfd
Alex Rivard <alex.rivard4406@gmail.com>
2024-11-21 09:35:26 -0600

    preferredDays has been moved to form part one

--- src/components/Form/FormPartTwo.jsx
+++ src/components/Form/FormPartTwo.jsx
@@ -116,28 +116,6 @@ const FormPartTwo = () => {
         </div>
 
         <div className="mb-3">
-          <label css={labelStyle}>Preferred Days *</label>
-          <select
-            name="preferred_days"
-            value={formValues.preferred_days}
-            className="form-select"
-            onChange={handleChange}
-            required
-          >
-            <option value="">Select Preferred Days</option>
-            <option value="Monday/Thursdays">Monday/Thursdays</option>
-            <option value="Tuesday/Fridays">Tuesday/Fridays</option>
-            <option value="Mondays">Mondays</option>
-            <option value="Tuesdays">Tuesdays</option>
-            <option value="Thursdays">Thursdays</option>
-            <option value="Fridays">Fridays</option>
-          </select>
-          <small css={descriptionStyle}>
-            Requests for twice-weekly practices and meetings will be reserved on Mon/Thur or Tue/Fri.
-          </small>
-        </div>
-
-        <div className="mb-3">
           <label css={labelStyle}>Start Date for Your Rental *</label>
           <input
             type="date"
