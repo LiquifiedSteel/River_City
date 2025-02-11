4d5c1bc9c0ada573a1baecf32db8b7fa7e8b8522
Alex Rivard <alex.rivard4406@gmail.com>
2025-02-11 12:15:31 -0600

    adminUsers initial setup completed

---
+++ src/components/Admin/AdminUsers/AdminUsers.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState} from "react";
+import { Table } from "react-bootstrap";
+import axios from "axios";
+
+function AdminUsers() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetchUsers(); // Invoke the fetch function
+    }, [])
+
+    const toggleAdmin = async (id) => {
+        try {
+            await axios.put(`/api/user/admin/${id}`);
+            fetchUsers();
+        } catch (err) {
+            console.error("Error changing user's admin status:" , err)
+        }
+    }
+
+    const fetchUsers = async () => {
+        try {
+          // Fetch applications data
+          const response = await axios.get("/api/user/users");
+          setUsers(response.data);
+        } catch (error) {
+          console.error("Error fetching Envelopes:", error);
+        }
+    };
+
+    return (
+        <div>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Is Admin</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.filter((user)=> !user.deleted).map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>{String(user.isAdmin)}</td>
+                            <td><button onClick={() => toggleAdmin(user.id)}>Toggle Admin</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
+    )
+
+}
+
+export default AdminUsers;
