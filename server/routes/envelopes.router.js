a0b859da401d473bc3666581d04fc16632fca109
Alex Rivard <alex.rivard4406@gmail.com>
2025-02-13 13:19:17 -0600

    envelopes router now has an admin editing route

--- server/routes/envelopes.router.js
+++ server/routes/envelopes.router.js
@@ -1,20 +1,20 @@
 const express = require("express"); // Import the Express framework to handle routing and HTTP requests.
 const pool = require("../modules/pool"); // Import the database pool to handle SQL queries.
 const {
-  rejectUnauthenticated,
+    rejectUnauthenticated,
 } = require("../modules/authentication-middleware"); // Middleware to reject unauthenticated users.
 const router = express.Router(); // Create a new Express router to define routes.
 
 router.get("/", rejectUnauthenticated, (req,res) => {
-  const queryText = `SELECT * FROM "Envelopes" GROUP BY "envelope";`;
+    const queryText = `SELECT * FROM "Envelopes" ORDER BY "id" ASC;`;
 
-  pool
-    .query(queryText)
-    .then((response) => res.send(response.rows).status(200))
-    .catch((err) => {
-      console.error("Failed to collect Envelopes: ", err);
-      res.sendStatus(400);
-    })
+    pool
+      .query(queryText)
+      .then((response) => res.send(response.rows).status(200))
+      .catch((err) => {
+        console.error("Failed to collect Envelopes: ", err);
+        res.sendStatus(400);
+      })
 })
 
 router.post("/add/", rejectUnauthenticated, (req, res) => {
@@ -31,28 +31,44 @@ router.post("/add/", rejectUnauthenticated, (req, res) => {
       })
 })
 
+router.put("/edit/", rejectUnauthenticated, (req,res) => {
+    const envelope = req.body.envelope;
+    const amount = req.body.total;
+    const id = req.body.id;
+
+    const queryText = `UPDATE "Envelopes" SET "envelope"=$1, "total"=$2 WHERE "id"=$3;`;
+
+    pool
+      .query(queryText, [envelope, amount, id])
+      .then(()=>res.sendStatus(200))
+      .catch((err) => {
+        console.error("Failed to update envelope: ", err);
+        res.sendStatus(400);
+      })
+})
+
 router.get("/navBudget", rejectUnauthenticated, (req, res) => {
-  const queryText = `SELECT * FROM "Budget" ORDER BY "id" ASC;`;
-  pool
-    .query(queryText)
-    .then((response) => res.send(response.rows).status(200))
-    .catch((err) => {
-      console.error("Failed to collect Budget Items: ", err);
-      res.sendStatus(400);
-    })
+    const queryText = `SELECT * FROM "Budget" ORDER BY "id" ASC;`;
+    pool
+      .query(queryText)
+      .then((response) => res.send(response.rows).status(200))
+      .catch((err) => {
+        console.error("Failed to collect Budget Items: ", err);
+        res.sendStatus(400);
+      })
 })
 
 router.put("/budget/:amount", rejectUnauthenticated, (req, res) => {;
-  const update = req.params.amount
-  const queryText = `UPDATE "Budget" SET "amount"=$1 WHERE "type"='Budget';`;
+    const update = req.params.amount
+    const queryText = `UPDATE "Budget" SET "amount"=$1 WHERE "type"='Budget';`;
 
-  pool
-    .query(queryText, [update])
-    .then(() => res.sendStatus(200))
-    .catch((err) => {
-      console.error("Failed to update annual Budget: ", err);
-      res.sendStatus(400);
-    })
+    pool
+      .query(queryText, [update])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        console.error("Failed to update annual Budget: ", err);
+        res.sendStatus(400);
+      })
 })
 
 
